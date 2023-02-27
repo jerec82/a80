a062909956c9e0aa1483037f2a313cfb74098627
Eugene Ovsyannikov <jerec82@mail.ru>
2023-02-27 12:45:36 +0500

    two commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { Collapse, Space, Tabs } from 'antd';
+import './App.css';
+
+const {Panel} = Collapse;
+
+function App() {
+  
+
+  
+  return (
+    <div className="App">
+
+      <Tabs 
+        type='card' 
+        defaultActiveKey='1'
+        items={[
+
+          {
+            label: 'Форма просмотра жалоб',
+            key: '1',
+            children: 
+            <>
+            
+              <Collapse>
+                <Panel header="Параметры" key="1">
+      
+                </Panel>
+                <Panel header="Результаты поиска" key="2">
+      
+                </Panel>
+              </Collapse>
+
+            </>
+
+          },
+          {
+            label: 'Вторая форма',
+            key: '2',
+            children:
+            <>
+            </>
+          },
+
+        ]}>
+      
+
+      </Tabs>
+
+
+    </div>
+  );
+}
+
+export default App;
